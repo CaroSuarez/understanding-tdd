ef6b953941a31c8818fc5a200c12a1d38918a5a2
CaroSuarez <simply.caro.developer@gmail.com>
2023-11-13 14:45:06 -0500

    refacto: rockPaperScissors refactor

--- katas/rock-paper-scissors.ts
+++ katas/rock-paper-scissors.ts
@@ -10,27 +10,56 @@ export enum Result {
   PLAYER_WINS = "Player wins",
 }
 
+export interface GameCase {
+  PLAYER_MOVE: Show;
+  OPPONENT_MOVE: Show;
+  RESULT: Result;
+}
+
+export const NO_TIE_GAME_CASES: GameCase[] = [
+  {
+    PLAYER_MOVE: Show.ROCK,
+    OPPONENT_MOVE: Show.SCISSORS,
+    RESULT: Result.PLAYER_WINS,
+  },
+  {
+    PLAYER_MOVE: Show.PAPER,
+    OPPONENT_MOVE: Show.ROCK,
+    RESULT: Result.PLAYER_WINS,
+  },
+  {
+    PLAYER_MOVE: Show.SCISSORS,
+    OPPONENT_MOVE: Show.PAPER,
+    RESULT: Result.PLAYER_WINS,
+  },
+  {
+    PLAYER_MOVE: Show.ROCK,
+    OPPONENT_MOVE: Show.PAPER,
+    RESULT: Result.PLAYER_LOSES,
+  },
+  {
+    PLAYER_MOVE: Show.PAPER,
+    OPPONENT_MOVE: Show.SCISSORS,
+    RESULT: Result.PLAYER_LOSES,
+  },
+  {
+    PLAYER_MOVE: Show.SCISSORS,
+    OPPONENT_MOVE: Show.ROCK,
+    RESULT: Result.PLAYER_LOSES,
+  },
+];
+
 export const rockPaperScissors = (playerMove: string, opponentMove: string) => {
   const tie = playerMove === opponentMove;
-  const playerLoses =
-    (playerMove === Show.ROCK && opponentMove === Show.PAPER) ||
-    (playerMove === Show.PAPER && opponentMove === Show.SCISSORS) ||
-    (playerMove === Show.SCISSORS && opponentMove === Show.ROCK);
-
-  const playerWins =
-    (playerMove === Show.ROCK && opponentMove === Show.SCISSORS) ||
-    (playerMove === Show.PAPER && opponentMove === Show.ROCK) ||
-    (playerMove === Show.SCISSORS && opponentMove === Show.PAPER);
+  const gameCase = NO_TIE_GAME_CASES.find(
+    (gameCase) =>
+      gameCase.PLAYER_MOVE === playerMove &&
+      gameCase.OPPONENT_MOVE === opponentMove
+  );
 
   if (tie) {
     return Result.TIE;
   }
-  if (playerLoses) {
-    return Result.PLAYER_LOSES;
-  }
-  if (playerWins) {
-    return Result.PLAYER_WINS;
-  }
 
-  return Result.TIE;
+  return gameCase?.RESULT;
 };
